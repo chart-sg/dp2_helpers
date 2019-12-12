a1157a4e30066fa19846e5f6910ef18611396681
Boon Han <cnboonhan94@gmail.com>
2019-12-12 21:21:32 +0700

    add arguments

--- ros2_heartbeat_publisher.py
+++ ros2_heartbeat_publisher.py
@@ -1,12 +1,30 @@
 import rclpy
+import argparse
 
 from std_msgs.msg import String
 
 
 def main(args=None):
+    '''
+    10.233.29.42, ChartServer
+    10.233.29.64, TrolleyBed
+    10.233.29.70, OTWorkcell
+    10.233.29.71, CSSDDoor
+    10.233.29.72, LiftController
+    10.233.29.73, MIR
+    10.233.29.74, Magni4
+    10.233.29.75, Magni2
+    10.233.29.76, MagniFleetManager
+    10.233.29.77, Magni3
+    '''
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--name', default='HumanRmfCore')
+    x = parser.parse_args()
+
     rclpy.init(args=args)
 
-    node = rclpy.create_node('HumanRmfCore_heartbeat')
+    name = x.name
+    node = rclpy.create_node(name + '_heartbeat')
     publisher = node.create_publisher(String, 'heartbeat', 10)
 
     msg = String()
@@ -14,7 +32,7 @@ def main(args=None):
 
     def timer_callback():
         nonlocal i
-        msg.data = 'HumanRmfCore'
+        msg.data = name
         node.get_logger().info('Publishing: "%s"' % msg.data)
         publisher.publish(msg)
 
