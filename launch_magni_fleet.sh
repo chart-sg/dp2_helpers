719d5fdeb4c91cfcf22b3b2286bc1efbb1464294
Boon Han <cnboonhan94@gmail.com>
2019-12-12 17:33:58 +0700

    first commit

---
+++ launch_magni_fleet.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+set -e
+
+export ROS_IP=10.233.29.76  # Your IP HERE
+gnome-terminal -- bash -c "echo 'Launching $1'; ssh $1; exec bash"
+export ROS_MASTER_URI=http://$1.local:11311
+source /opt/ros/melodic/setup.bash
+rviz &
