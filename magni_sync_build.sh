cec0be287b6da722e4b2fdf011798f4ca09e484e
Boon Han <cnboonhan94@gmail.com>
2019-12-14 16:31:32 +0700

    update

--- magni_sync_build.sh
+++ magni_sync_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd ~/dev/dp2_ws && source /opt/ros/eloquent/setup.bash && colcon build
-#ssh -t ubuntu@magni1.local "cd magni_ws && source /opt/ros/melodic/setup.bash && catkin build"
-ssh -t ubuntu@magni2.local "cd magni_ws && source /opt/ros/melodic/setup.bash && catkin build"
-ssh -t ubuntu@magni3.local "cd magni_ws && source /opt/ros/melodic/setup.bash && catkin build"
-#ssh -t ubuntu@magni4.local "cd magni_ws && source /opt/ros/melodic/setup.bash && catkin build"
+#ssh -t ubuntu@magni1.local "cd magni_ws && source /opt/ros/melodic/setup.bash && build devel logs &&catkin build"
+ssh -t ubuntu@magni2.local "cd magni_ws && source /opt/ros/melodic/setup.bash && rm -rf build devel logs && catkin build"
+ssh -t ubuntu@magni3.local "cd magni_ws && source /opt/ros/melodic/setup.bash && rm -rf build devel logs && catkin build"
+#ssh -t ubuntu@magni4.local "cd magni_ws && source /opt/ros/melodic/setup.bash && rm -rf build devel logs && catkin build"
