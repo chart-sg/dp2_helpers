c604da488dd567c2283de714f6959e13f178306a
Boon Han <cnboonhan94@gmail.com>
2019-12-14 13:53:14 +0700

    add batch modification of common config files

---
+++ magni_batch_modify_configs.sh
@@ -0,0 +1,5 @@
+# run in repos folder
+vim free_fleet/servers/ros2/free_fleet_server/launch/test_server.yaml \
+    rmf_dp2/magni_dp2/launch/magni_free_fleet_client.launch \
+    rmf_dp2/magni_dp2/launch/magni_nuc_chart.launch \
+    ~/.bashrc
---
+++ magni_patch_rplidar_driver.sh
@@ -0,0 +1,2 @@
+#run in repos folder
+sed -i s/#define\ DEFAULT_MOTOR_PWM\ *660/#define\ DEFAULT_MOTOR_PWM\ 1000/ rplidar_ros/sdk/include/rplidar_cmd.h
