041a916fb29b1e658a7aa098a85b81311cbb6944
Boon Han <cnboonhan94@gmail.com>
2019-12-17 12:41:19 +0700

    update

--- magni_batch_modify_configs.sh
+++ magni_batch_modify_configs.sh
@@ -2,4 +2,7 @@
 vim free_fleet/servers/ros2/free_fleet_server/launch/test_server.yaml \
     rmf_dp2/magni_dp2/launch/magni_free_fleet_client.launch \
     rmf_dp2/magni_dp2/launch/magni_nuc_chart.launch \
+    rmf_dp2/dp2/launch/demo.launch.xml \
+    rmf_dp2/dp2/launch/include/magni_adapter.launch.xml \
+    rmf_dp2/dp2/launch/include/mir_adapter.launch.xml \
     ~/.bashrc
--- magni_sync_build.sh
+++ magni_sync_build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ~/dev/dp2_ws && source /opt/ros/eloquent/setup.bash && colcon build
+cd ~/dev/dp2_ws && rm -rf build log install && source /opt/ros/eloquent/setup.bash && colcon build
 #ssh -t ubuntu@magni1.local "cd magni_ws && source /opt/ros/melodic/setup.bash && build devel logs &&catkin build"
 ssh -t ubuntu@magni2.local "cd magni_ws && source /opt/ros/melodic/setup.bash && rm -rf build devel logs && catkin build"
 ssh -t ubuntu@magni3.local "cd magni_ws && source /opt/ros/melodic/setup.bash && rm -rf build devel logs && catkin build"
