c82820871533062297b9f50bf797b5f543bfd5cf
Boon Han <cnboonhan94@gmail.com>
2019-12-18 10:39:08 +0700

    update

--- door-lift-bridge.py
+++ door-lift-bridge.py
@@ -10,10 +10,10 @@ class DoorLiftAdapter(Node):
 
     def __init__(self):
         super().__init__('door_lift_bridge')
-        lift_adapter_subscriber = self.create_subscription(DoorRequest, 'door_requests', self.lift_adapter_subscriber_callback, 10)
-        door_adapter_subscriber = self.create_subscription(LiftState, 'lift_states', self.door_adapter_subscriber_callback, 10)
-        lift_adapter_publisher = self.create_publisher(LiftRequest, 'lift_requests', 10)
-        door_adapter_publisher = self.create_publisher(DoorState, 'door_states', 10)
+        self.lift_adapter_subscriber = self.create_subscription(DoorRequest, 'door_requests', self.lift_adapter_subscriber_callback, 10)
+        self.door_adapter_subscriber = self.create_subscription(LiftState, 'lift_states', self.door_adapter_subscriber_callback, 10)
+        self.lift_adapter_publisher = self.create_publisher(LiftRequest, 'lift_requests', 10)
+        self.door_adapter_publisher = self.create_publisher(DoorState, 'door_states', 10)
 
     def door_adapter_subscriber_callback(self, msg):
         print("translating lift_states to door_states")
@@ -21,18 +21,19 @@ class DoorLiftAdapter(Node):
         pass
     
     def lift_adapter_subscriber_callback(self, in_msg):
-        print("translating door_requests to lift_requests")
 	# Check if message is for door LF001
-	# if so:
-        out_msg = LiftRequest()
-        out_msg.request_time = self._node.get_clock().now().to_msg()
-        out_msg.session_id = "lift_door_bridge"
-        out_msg.lift_name = "LF001"
-        out_msg.destination_floor = 'L3'
-        out_msg.request_type = out_msg.REQUEST_AGV_MODE
-        out_msg.door_state = out_msg.DOOR_CLOSED
-        print(out_msg)
-        self.lift_requests_pub.publish(out_msg)
+	# process in_msg door_requests to lift_requests
+        if in_msg.requester_id == "MiR_R1442" and in_msg.door_name == "chart_cssd_door": # and in_msg.door_name == "LF001": # verify this is correct requester_id
+            print("received lift_request from MiR_R1442, translating")
+            out_msg = LiftRequest()
+            out_msg.request_time = self.get_clock().now().to_msg()
+            out_msg.session_id = "lift_door_bridge"
+            out_msg.lift_name = "LF001"
+            out_msg.destination_floor = 'L3'
+            out_msg.request_type = out_msg.REQUEST_AGV_MODE
+            out_msg.door_state = out_msg.DOOR_CLOSED
+            print(out_msg)
+            self.lift_adapter_publisher.publish(out_msg)
         # can extend to more scenarios, perhaps based on session id name
 
 def main(args=None):
