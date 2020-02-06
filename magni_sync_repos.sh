f31ef0b4e2bbd80835667fce238664bca2c8ef5f
Boon Han <cnboonhan94@gmail.com>
2020-02-06 15:29:50 +0800

    add magni1

--- magni_sync_repos.sh
+++ magni_sync_repos.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#rsync -a ~/dp2_ws/ ubuntu@magni1.local:~/dp2_ws
+rsync -a ~/dp2_ws/ ubuntu@magni1.local:~/dp2_ws
 rsync -a ~/dp2_ws/ ubuntu@magni2.local:~/dp2_ws
 #rsync -a ~/dp2_ws/ ubuntu@magni3.local:~/dp2_ws
 #rsync -a ~/dp2_ws/ ubuntu@magni4.local:~/dp2_ws
