00d5fd3ba2dc522a7303749fcb9ac2a4ca49d62c
Boon Han <cnboonhan94@gmail.com>
2020-02-06 15:00:57 +0800

    update

--- watch_dp2_requests.sh
+++ watch_dp2_requests.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-tmux send 'ffs_env && ros2 topic echo dispenser_requests' ENTER;
+
+tmux send 'source /opt/ros/eloquent/setup.bash && source ~/dp2_ws/ros2/install/setup.bash && ros2 topic echo dispenser_requests' ENTER;
 tmux split-window  -v
-tmux send 'ffs_env && ros2 topic echo door_requests' ENTER;
+tmux send 'source /opt/ros/eloquent/setup.bash && source ~/dp2_ws/ros2/install/setup.bash && ros2 topic echo door_requests' ENTER;
 tmux split-window -v 
-tmux send 'ffs_env && ros2 topic echo lift_requests' ENTER;
+tmux send 'source /opt/ros/eloquent/setup.bash && source ~/dp2_ws/ros2/install/setup.bash && ros2 topic echo lift_requests' ENTER;
 tmux split-window -v 
-tmux send 'ffs_env && ros2 topic echo robot_path_requests' ENTER;
+tmux send 'source /opt/ros/eloquent/setup.bash && source ~/dp2_ws/ros2/install/setup.bash && ros2 topic echo robot_path_requests' ENTER;
 tmux select-layout even-horizontal
--- watch_dp2_states.sh
+++ watch_dp2_states.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-tmux send 'ffs_env && ros2 topic echo dispenser_states' ENTER;
+tmux send 'source /opt/ros/eloquent/setup.bash && source ~/dp2_ws/ros2/install/setup.bash && ros2 topic echo dispenser_states' ENTER;
 tmux split-window  -v
-tmux send 'ffs_env && ros2 topic echo door_states' ENTER;
+tmux send 'source /opt/ros/eloquent/setup.bash && source ~/dp2_ws/ros2/install/setup.bash && ros2 topic echo door_states' ENTER;
 tmux split-window -v 
-tmux send 'ffs_env && ros2 topic echo lift_states' ENTER;
+tmux send 'source /opt/ros/eloquent/setup.bash && source ~/dp2_ws/ros2/install/setup.bash && ros2 topic echo lift_states' ENTER;
 tmux split-window -v 
-tmux send 'ffs_env && ros2 topic echo fleet_states' ENTER;
-tmux split-window -v 
-tmux send 'ffs_env && cd /home/bhan/repos/dp2_helpers && ./ping_check.sh' ENTER;
+tmux send 'source /opt/ros/eloquent/setup.bash && source ~/dp2_ws/ros2/install/setup.bash && ros2 topic echo fleet_states' ENTER;
 tmux select-layout even-horizontal
