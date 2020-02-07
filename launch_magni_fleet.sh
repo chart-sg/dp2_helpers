e5725351f2b1f574d30b546cbe364ec71e3063bd
Boon Han <cnboonhan94@gmail.com>
2020-02-07 14:40:33 +0800

    dont launch rviz as background

--- launch_magni_fleet.sh
+++ launch_magni_fleet.sh
@@ -7,4 +7,4 @@ export ROS_IP=10.233.29.76  # Your IP HERE
 gnome-terminal -- bash -c "echo 'Launching $1'; ssh $1; exec bash"
 export ROS_MASTER_URI=http://$1.local:11311
 source /opt/ros/melodic/setup.bash
-rviz &
+rviz 
