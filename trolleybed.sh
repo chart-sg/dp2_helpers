29c541020549a7011f5f420f448fbb95676bb5da
kerliang0801 <christopherlaukl@gmail.com>
2020-02-07 16:26:07 +0800

    fix trolleybed.sh

--- trolleybed.sh
+++ trolleybed.sh
@@ -7,7 +7,7 @@ tmux send "ssh trolleybed './start_map_server.sh'" ENTER;
 tmux split-window  -v
 tmux send "ssh trolleybed './start_trolley_bed.sh'" ENTER;
 tmux split-window  -v
-tmux send "ssh trolleybed 'ros2_load; ros2 run smart_trolley_bed smart_trolley_bed Bed001 Hope BedFleetA __ns:=/BedFleetA/Bed001;'" ENTER;
+tmux send "ssh trolleybed '. /opt/ros/dashing/setup.bash; . /home/ubuntu/ros2_ws/install/setup.bash; ros2 run smart_trolley_bed smart_trolley_bed Bed001 Hope BedFleetA __ns:=/BedFleetA/Bed001;'" ENTER;
 tmux split-window  -v
 tmux send 'ssh trolleybed' ENTER;
 tmux select-layout even-vertical
