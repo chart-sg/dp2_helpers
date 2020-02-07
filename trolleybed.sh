65f1869097ae46b9b18e23517db144b0dd3f26ef
kerliang0801 <christopherlaukl@gmail.com>
2020-02-07 16:04:38 +0800

    launching bash scripts from ssh for the trolleybed

--- trolleybed.sh
+++ trolleybed.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 
+tmux send 'ssh trolleybed './start_bolt_on_kit.sh'' ENTER;
+tmux split-window  -v
+tmux send 'ssh trolleybed './start_map_server.sh'' ENTER;
+tmux split-window  -v
+tmux send 'ssh trolleybed './start_trolley_bed.sh'' ENTER;
+tmux split-window  -v
+tmux send 'ssh trolleybed 'ros2_load; ros2 run smart_trolley_bed smart_trolley_bed Bed001 Hope BedFleetA __ns:=/BedFleetA/Bed001;'' ENTER;
 tmux send 'ssh trolleybed' ENTER;
 tmux split-window  -v
-tmux send 'ssh trolleybed' ENTER;
-tmux split-window  -v
-tmux send 'ssh trolleybed' ENTER;
-tmux split-window  -v
-tmux send 'ssh trolleybed' ENTER;
 tmux select-layout even-vertical
